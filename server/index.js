707a9fff247301b5c92a879a749abab39f0fc2c2
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-17 23:03:56 -0800

    Allow cors for Sockets

--- server/index.js
+++ server/index.js
@@ -1,119 +1,130 @@
-const Session = require('./session');
-const Client = require('./client');
-const SpyGame = require('./spy');
+const Session = require("./session");
+const Client = require("./client");
+const SpyGame = require("./spy");
 
 // Express static site
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
-const clientPath = 'build';
-app.use(express.static(clientPath), express.static('node_modules/bootstrap/dist'));
+const clientPath = "build";
+app.use(
+  express.static(clientPath),
+  express.static("node_modules/bootstrap/dist")
+);
 
 // socket.io
-const io = require('socket.io')(http);
-const sessions = new Map;
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://distracted-villani-c928ee.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
+const sessions = new Map();
 
-function createId(len = 8, chars = 'ABCDEFGHJKMNPQRSTWXYZ23456789') {
-    let id = '';
-    for (let i = 0; i < len; i++) {
-        id += chars[Math.random() * chars.length | 0];
-    }
-    return id;
+function createId(len = 8, chars = "ABCDEFGHJKMNPQRSTWXYZ23456789") {
+  let id = "";
+  for (let i = 0; i < len; i++) {
+    id += chars[(Math.random() * chars.length) | 0];
+  }
+  return id;
 }
 
 function createClient(socket) {
-    return new Client(socket);
+  return new Client(socket);
 }
 
 function createSession(id = createId(5)) {
-    if (sessions.has(id)) {
-        console.error(`Session ${id} already exists`);
-        return null;
-    }
-    let session = new Session(id, io);
-    sessions.set(id, session);
-    return session;
+  if (sessions.has(id)) {
+    console.error(`Session ${id} already exists`);
+    return null;
+  }
+  let session = new Session(id, io);
+  sessions.set(id, session);
+  return session;
 }
 
 function getSession(id) {
-    return sessions.get(id);
+  return sessions.get(id);
 }
 
-io.on('connection', socket => {
-    const client = new createClient(socket);
-    let session;
+io.on("connection", (socket) => {
+  const client = new createClient(socket);
+  let session;
 
-    socket.on('join-session', data => {
-        if (data.sessionId) {
-            session = getSession(data.sessionId) || createSession(data.sessionId);
-        } else {
-            session = createSession();
-        }
-        if (session) {
-            console.log('Created session:', session.id);
-            client.name = data.playerName;
-            if (session.join(client)) {
-                session.broadcastPeers();
-            } else {
-                socket.disconnect();
-            }
-        }
-    });
+  socket.on("join-session", (data) => {
+    if (data.sessionId) {
+      session = getSession(data.sessionId) || createSession(data.sessionId);
+    } else {
+      session = createSession();
+    }
+    if (session) {
+      console.log("Created session:", session.id);
+      client.name = data.playerName;
+      if (session.join(client)) {
+        session.broadcastPeers();
+      } else {
+        socket.disconnect();
+      }
+    }
+  });
 
-    socket.on('chat-event', data => {
-        if (!session) {
-            socket.disconnect();
-        } else {
-            // TODO only broadcast to other clients
-            session.broadcast('chat-event', {
-                id: client.id,
-                author: client.name,
-                message: data.message
-            });
-        }
-    });
+  socket.on("chat-event", (data) => {
+    if (!session) {
+      socket.disconnect();
+    } else {
+      // TODO only broadcast to other clients
+      session.broadcast("chat-event", {
+        id: client.id,
+        author: client.name,
+        message: data.message,
+      });
+    }
+  });
 
-    socket.on('player-ready', data => {
-        if (!session) {
-            socket.disconnect();
-        } else {
-            client.ready = data.ready;
-            session.broadcastPeers();
-        }
-    });
+  socket.on("player-ready", (data) => {
+    if (!session) {
+      socket.disconnect();
+    } else {
+      client.ready = data.ready;
+      session.broadcastPeers();
+    }
+  });
 
-    socket.on('start-game', () => {
-        if (!session) {
-            socket.disconnect();
-        } else {
-            let allReady = Array.from(session.clients).reduce((acc, cli) => acc && cli.ready, true);
-            if (allReady) {
-                SpyGame.startGame(session);
-            } else {
-                client.send('chat-event', {
-                    message: 'All players must be ready',
-                    color: 'red',
-                });
-            }
-        }
-    });
+  socket.on("start-game", () => {
+    if (!session) {
+      socket.disconnect();
+    } else {
+      let allReady = Array.from(session.clients).reduce(
+        (acc, cli) => acc && cli.ready,
+        true
+      );
+      if (allReady) {
+        SpyGame.startGame(session);
+      } else {
+        client.send("chat-event", {
+          message: "All players must be ready",
+          color: "red",
+        });
+      }
+    }
+  });
 
-    socket.on('disconnect', () => {
-        if (session) {
-            session.leave(client);
-            if (session.clients.size === 0) {
-                sessions.delete(session.id);
-                console.log('Sessions remaining:', sessions.size);
-            } else {
-                session.broadcastPeers();
-            }
-        }
-    });
+  socket.on("disconnect", () => {
+    if (session) {
+      session.leave(client);
+      if (session.clients.size === 0) {
+        sessions.delete(session.id);
+        console.log("Sessions remaining:", sessions.size);
+      } else {
+        session.broadcastPeers();
+      }
+    }
+  });
 });
 
 const defaultPort = 8081;
 const actualPort = process.env.PORT || defaultPort;
 http.listen(actualPort, () => {
-    console.log(`Serving Online Spy on http://localhost:${actualPort}`);
+  console.log(`Serving Online Spy on http://localhost:${actualPort}`);
 });
