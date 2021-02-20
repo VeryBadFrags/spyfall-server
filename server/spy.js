f90027b62bacefc07e2ebe08328cb7d1e3b4264c
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-20 09:56:48 -0800

    Add basic support for extended locations

--- server/spy.js
+++ server/spy.js
@@ -1,4 +1,4 @@
-const locationsList = [
+const locations = [
   "✈️💺 Airport",
   "🏦💰 Bank",
   "🎰💵 Casino",
@@ -17,20 +17,31 @@ const locationsList = [
   "🏫🎓 University",
 ];
 
-function startGame(session) {
-  let location =
-    locationsList[Math.floor(Math.random() * locationsList.length)];
+const extendedLocations = [
+  "🏝🥥 Desert Island",
+  "⛰🥾 Mountain Hike",
+  "🏤📮 Post Office",
+  "🍽👩‍🍳 Restaurant",
+];
+
+function startGame(session, extendedMode) {
   let clientsArray = Array.from(session.clients);
   let spyIndex = Math.floor(Math.random() * clientsArray.length);
   let firstQuestion =
     clientsArray[Math.floor(Math.random() * clientsArray.length)].name;
+
+  let gameLocations = extendedMode
+    ? [...locations, ...extendedLocations]
+    : locations;
+  let currentLocation = gameLocations[Math.floor(Math.random() * locations.length)];
+
   clientsArray.forEach((client, index) => {
     let isSpy = spyIndex === index;
     client.ready = false;
     client.send("start-game", {
       spy: isSpy,
-      location: isSpy ? "?" : location,
-      locations: locationsList,
+      location: isSpy ? "?" : currentLocation,
+      locations: gameLocations,
       first: firstQuestion,
     });
   });
