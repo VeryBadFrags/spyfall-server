3b03f1957b487e31e472b06aa8bd78ef2f46f1f2
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2022-04-09 13:57:32 -0700

    Replace Standard with ESLint

--- server/index.js
+++ server/index.js
@@ -1,143 +1,143 @@
-const Session = require('./session')
-const Client = require('./client')
-const SpyGame = require('./spy')
+const Session = require("./session");
+const Client = require("./client");
+const SpyGame = require("./spy");
 
-const http = require('http').createServer()
+const http = require("http").createServer();
 
 const corsOptions = {
   cors: {
     origin: [
-      'https://spy.verybadfrags.com',
-      'https://heuristic-bartik-850df8.netlify.app'
+      "https://spy.verybadfrags.com",
+      "https://heuristic-bartik-850df8.netlify.app",
     ],
-    methods: ['GET', 'POST']
-  }
-}
+    methods: ["GET", "POST"],
+  },
+};
 
-const nodeEnv = process.env.NODE_ENV
-console.log(`NODE_ENV=${nodeEnv}`)
+const nodeEnv = process.env.NODE_ENV;
+console.log(`NODE_ENV=${nodeEnv}`);
 
-const localFrontEnd = 'http://localhost:3000'
-if (nodeEnv === 'dev') {
-  console.log(`Allowing cors for ${localFrontEnd}`)
-  corsOptions.cors.origin.push(localFrontEnd)
+const localFrontEnd = "http://localhost:3000";
+if (nodeEnv === "dev") {
+  console.log(`Allowing cors for ${localFrontEnd}`);
+  corsOptions.cors.origin.push(localFrontEnd);
 }
 
 // socket.io
-const io = require('socket.io')(http, corsOptions)
-const sessions = new Map()
+const io = require("socket.io")(http, corsOptions);
+const sessions = new Map();
 
-function createId (len = 8, chars = 'ABCDEFGHJKMNPQRSTWXYZ23456789') {
-  let id = ''
+function createId(len = 8, chars = "ABCDEFGHJKMNPQRSTWXYZ23456789") {
+  let id = "";
   for (let i = 0; i < len; i++) {
-    id += chars[(Math.random() * chars.length) | 0]
+    id += chars[(Math.random() * chars.length) | 0];
   }
-  return id
+  return id;
 }
 
-function createClient (socket) {
-  return new Client(socket)
+function createClient(socket) {
+  return new Client(socket);
 }
 
-function createSession (id = createId(5)) {
+function createSession(id = createId(5)) {
   if (sessions.has(id)) {
-    console.error(`Session ${id} already exists`)
-    return null
+    console.error(`Session ${id} already exists`);
+    return null;
   }
-  const session = new Session(id, io)
-  sessions.set(id, session)
-  return session
+  const session = new Session(id, io);
+  sessions.set(id, session);
+  return session;
 }
 
-function getSession (id) {
-  return sessions.get(id)
+function getSession(id) {
+  return sessions.get(id);
 }
 
-io.on('connection', (socket) => {
-  const client = createClient(socket)
-  let session
+io.on("connection", (socket) => {
+  const client = createClient(socket);
+  let session;
 
-  socket.on('join-session', (data) => {
+  socket.on("join-session", (data) => {
     if (session) {
-      leaveSession(session, client)
+      leaveSession(session, client);
     }
     if (data.sessionId) {
-      session = getSession(data.sessionId) || createSession(data.sessionId)
+      session = getSession(data.sessionId) || createSession(data.sessionId);
     } else {
-      session = createSession()
+      session = createSession();
     }
-    client.send('session-created', { sessionId: session.id })
+    client.send("session-created", { sessionId: session.id });
     if (session) {
-      console.log('Created session:', session.id)
-      client.name = data.playerName
+      console.log("Created session:", session.id);
+      client.name = data.playerName;
       if (session.join(client)) {
-        session.broadcastPeers()
+        session.broadcastPeers();
       } else {
-        socket.disconnect()
+        socket.disconnect();
       }
     }
-  })
+  });
 
-  socket.on('chat-event', (data) => {
+  socket.on("chat-event", (data) => {
     if (!session) {
-      socket.disconnect()
+      socket.disconnect();
     } else {
       // TODO only broadcast to other clients
-      session.broadcast('chat-event', {
+      session.broadcast("chat-event", {
         id: client.id,
         author: client.name,
-        message: data.message
-      })
+        message: data.message,
+      });
     }
-  })
+  });
 
-  socket.on('player-ready', (data) => {
+  socket.on("player-ready", (data) => {
     if (!session) {
-      socket.disconnect()
+      socket.disconnect();
     } else {
-      client.ready = data.ready
-      session.broadcastPeers()
+      client.ready = data.ready;
+      session.broadcastPeers();
     }
-  })
+  });
 
-  socket.on('start-game', () => {
+  socket.on("start-game", () => {
     if (!session) {
-      socket.disconnect()
+      socket.disconnect();
     } else {
       const allReady = Array.from(session.clients).reduce(
         (acc, cli) => acc && cli.ready,
         true
-      )
+      );
       if (allReady) {
-        SpyGame.startGame(session)
+        SpyGame.startGame(session);
       } else {
-        client.send('chat-event', {
-          message: 'All players must be ready',
-          color: 'red'
-        })
+        client.send("chat-event", {
+          message: "All players must be ready",
+          color: "red",
+        });
       }
     }
-  })
+  });
 
-  socket.on('disconnect', () => {
-    leaveSession(session, client)
-  })
-})
+  socket.on("disconnect", () => {
+    leaveSession(session, client);
+  });
+});
 
-function leaveSession (session, client) {
+function leaveSession(session, client) {
   if (session) {
-    session.leave(client)
+    session.leave(client);
     if (session.clients.size === 0) {
-      sessions.delete(session.id)
-      console.log('Sessions remaining:', sessions.size)
+      sessions.delete(session.id);
+      console.log("Sessions remaining:", sessions.size);
     } else {
-      session.broadcastPeers()
+      session.broadcastPeers();
     }
   }
 }
 
-const defaultPort = 8081
-const actualPort = process.env.PORT || defaultPort
+const defaultPort = 8081;
+const actualPort = process.env.PORT || defaultPort;
 http.listen(actualPort, () => {
-  console.log(`Listening for requests on http://localhost:${actualPort}`)
-})
+  console.log(`Listening for requests on http://localhost:${actualPort}`);
+});
--- server/session.js
+++ server/session.js
@@ -1,64 +1,80 @@
 class Session {
-  constructor (id, io) {
-    this.id = id
-    this.io = io
-    this.clients = new Set()
-    this.avatars = ['🐱', '🐶', '🦊', '🐭', '🐼', '🐧', '🐰', '🐯',
-      '🦖', '🦉', '🐻', '🐠', '🦩', '🐢', '🐬', '🦆']
+  constructor(id, io) {
+    this.id = id;
+    this.io = io;
+    this.clients = new Set();
+    this.avatars = [
+      "🐱",
+      "🐶",
+      "🦊",
+      "🐭",
+      "🐼",
+      "🐧",
+      "🐰",
+      "🐯",
+      "🦖",
+      "🦉",
+      "🐻",
+      "🐠",
+      "🦩",
+      "🐢",
+      "🐬",
+      "🦆",
+    ];
   }
 
-  join (client) {
+  join(client) {
     if (this.clients.has(client)) {
-      console.log('Error: Client already in session')
-      return false
+      console.log("Error: Client already in session");
+      return false;
     }
 
     if (this.avatars.length === 0) {
-      console.error('The game is full')
-      return false
+      console.error("The game is full");
+      return false;
     }
 
-    this.clients.add(client)
+    this.clients.add(client);
     // Join the socket.io room
-    client.joinRoom(this.id)
+    client.joinRoom(this.id);
 
-    const avatar = this.avatars.shift()
-    client.avatar = avatar
-    client.name = `${avatar} ${client.name}`
+    const avatar = this.avatars.shift();
+    client.avatar = avatar;
+    client.name = `${avatar} ${client.name}`;
 
-    client.send('session-created', {
-      sessionId: this.id
-    })
-    return true
+    client.send("session-created", {
+      sessionId: this.id,
+    });
+    return true;
   }
 
-  leave (client) {
-    this.clients.delete(client)
-    this.avatars.push(client.avatar)
+  leave(client) {
+    this.clients.delete(client);
+    this.avatars.push(client.avatar);
   }
 
-  broadcast (type, data) {
-    data.type = type
-    this.io.to(this.id).send(data)
+  broadcast(type, data) {
+    data.type = type;
+    this.io.to(this.id).send(data);
   }
 
-  broadcastPeers () {
-    const clientsArray = Array.from(this.clients)
+  broadcastPeers() {
+    const clientsArray = Array.from(this.clients);
     const payload = {
-      type: 'session-broadcast',
+      type: "session-broadcast",
       sessionId: this.id,
       peers: {
-        clients: clientsArray.map(cli => {
+        clients: clientsArray.map((cli) => {
           return {
             id: cli.id,
             name: cli.name,
-            ready: cli.ready
-          }
-        })
-      }
-    }
-    this.io.to(this.id).send(payload)
+            ready: cli.ready,
+          };
+        }),
+      },
+    };
+    this.io.to(this.id).send(payload);
   }
 }
 
-module.exports = Session
+module.exports = Session;
